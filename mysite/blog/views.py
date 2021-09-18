fc07d246340cbf55f640501711ddf8bf309ceaef
n-kulebyakin <nakulebyakin@yandex.ru>
2021-09-18 18:51:54 +0300

    search improved

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -119,8 +119,16 @@ def post_search(request):
         form = SearchForm(request.GET)
     if form.is_valid():
         query = form.cleaned_data['query']
-        results = Post.objects.annotate(search=SearchVector('title', 'body'),
-                                        ).filter(search=query)
+        search_vector = (SearchVector('title', weight='A') +
+                         SearchVector('body', weight='B'))
+        search_query = SearchQuery(query)
+        results = Post.objects.annotate(
+            rank=SearchRank(search_vector, search_query)
+        ).filter(rank__gte=0.3).order_by('-rank')
+
+
+        #results = Post.objects.annotate(search=SearchVector('title', 'body'),
+        #                                ).filter(search=query)
     return render(request, 'blog/post/search.html', {'form': form,
                                                          'query': query,
                                                          'results': results})
